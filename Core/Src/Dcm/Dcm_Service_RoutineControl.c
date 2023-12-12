619ce862a905533f51f8052826ce0afefbca15bd
913-Goian-Sergiu <sergiu_goian@yahoo.com>
2023-12-12 09:41:44 +0200

    flash

--- Core/Src/Dcm/Dcm_Service_RoutineControl.c
+++ Core/Src/Dcm/Dcm_Service_RoutineControl.c
@@ -77,7 +77,7 @@ uint8_t Dcm_Service_RoutineControl(uint8_t *requestMessageData, uint8_t requestM
 {
     //implementation
 	//BEGIN
-	if(requestMessageLength < 5)
+	if(requestMessageData[0] != DCM_SERVICE_ID_ROUTINE_CONTROL  ||  requestMessageLength < 5  ||  requestMessageLength > 8)
 		return INCORRECT_MESSAGE_LENGTH_OR_INVALID_FORMAT;
 
 	routineControlTable.routineId=(requestMessageData[2] << 8) | requestMessageData[3]; //form Routine ID
@@ -85,9 +85,9 @@ uint8_t Dcm_Service_RoutineControl(uint8_t *requestMessageData, uint8_t requestM
 		return REQUEST_OUT_OF_RANGE; //NRC-routine id incorrect
 
 	//for routineStatusRecord
-		uint8_t routineControlOptionRecord[4];
-		for(int i=0;i<4;i++)
-			routineControlOptionRecord[i]=requestMessageData[i+4];
+	uint8_t routineControlOptionRecord[4];
+	for(int i=0;i<4;i++)
+		routineControlOptionRecord[i]=requestMessageData[i+4];
 
 	switch(requestMessageData[1])
 	{
@@ -96,7 +96,7 @@ uint8_t Dcm_Service_RoutineControl(uint8_t *requestMessageData, uint8_t requestM
 			{
 				if(routineIds[i] == routineControlTable.routineId)
 				{
-					if(routineStatus[routineControlTable.routineId-1] != ROUTINE_ACTIVE)
+					if(routineStatus[i] != ROUTINE_ACTIVE)
 						routineControlTable.StartRoutine[i](responseData, START_ROUTINE, routineControlOptionRecord);
 					else
 						return REQUEST_SEQUENCE_ERROR; //NRC-daca rutina e deja activa
@@ -110,7 +110,7 @@ uint8_t Dcm_Service_RoutineControl(uint8_t *requestMessageData, uint8_t requestM
 				if(routineIds[i]==routineControlTable.routineId)
 				{
 					//check if routine is currently active, if not => NRC-REQUEST_SEQUENCE_ERROR
-					if(routineStatus[routineControlTable.routineId-1] == ROUTINE_ACTIVE)
+					if(routineStatus[i] == ROUTINE_ACTIVE)
 					{
 						routineControlTable.StopRoutine[i](responseData, STOP_ROUTINE, routineControlOptionRecord);
 					}
@@ -128,7 +128,7 @@ uint8_t Dcm_Service_RoutineControl(uint8_t *requestMessageData, uint8_t requestM
 				if(routineIds[i]==routineControlTable.routineId)
 				{
 					//check if routine was active, if not return NRC-REQUEST_SEQUENCE_ERROR
-					if(routineStatus[routineControlTable.routineId-1] != ROUTINE_NEVER_ACTIVE)
+					if(routineStatus[i] != ROUTINE_NEVER_ACTIVE)
 					{
 						routineControlTable.RequestRoutineResults[i](responseData, REQUEST_ROUTINE_RESULTS, routineControlOptionRecord);
 					}
