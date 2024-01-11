ac3cebc66c1430d02d8d7d240820cb970e6e4985
elirecan <146091807+elirecan@users.noreply.github.com>
2024-01-11 15:51:28 +0200

    NRC

--- Core/Src/Dcm/Dcm_Service_WriteDataByIdentifier.c
+++ Core/Src/Dcm/Dcm_Service_WriteDataByIdentifier.c
@@ -8,6 +8,7 @@
 
 extern Dcm_DID *allDIDs[100];
 
+//implementation service 0X2E
 uint8_t Dcm_Service_WriteDataByIdentifier(uint8_t *requestMessageData, uint8_t requestMessageLength, uint8_t *responseData, uint8_t *responseDataLength)
 {
 	if (requestMessageData[0] != DCM_SERVICE_ID_WRITE_DATA_BY_IDENTIFIER)
@@ -15,18 +16,23 @@ uint8_t Dcm_Service_WriteDataByIdentifier(uint8_t *requestMessageData, uint8_t r
 			return 0x00;
 		}
 
-    if(requestMessageLength < 4){
-    	responseData[0] = NEGATIVE_RESPONSE_SID;
-    	responseData[1] = 0x13;
+	//NRC 0X13
+	//the length of the message is wrong
+	if((requestMessageLength < 4) || (requestMessageLength > 8)){
+	    	responseData[0] = NEGATIVE_RESPONSE_SID;
+	    	responseData[1] = 0x13;
 
-    	return 0x00;
+	    	return 0x00;
 
-    }
+	    }
 
+	//create positive response
 	uint8_t responseDataIndex = 0;
     responseData[responseDataIndex++] = DCM_SERVICE_ID_WRITE_DATA_BY_IDENTIFIER_RESPONSE_SID;
 
     uint8_t requestDataIndex = 1;
+
+    //read dataIdentifier request parameter
     uint8_t didHighByte = requestMessageData[requestDataIndex++];
     uint8_t didLowByte = requestMessageData[requestDataIndex++];
     uint16_t did = ((uint16_t)didHighByte << 8) + didLowByte;
--- Core/Src/Dcm/Dcm_Service_WriteMemoryByAddress.c
+++ Core/Src/Dcm/Dcm_Service_WriteMemoryByAddress.c
@@ -7,6 +7,7 @@
 #include <Dcm_Service_WriteMemoryByAddress.h>
 
 
+//implementation service 0X3D
 uint8_t Dcm_Service_WriteMemoryByAddress(uint8_t *requestMessageData, uint8_t requestMessageLength, uint8_t *responseData, uint8_t *responseDataLength)
 {
 	uint8_t dataRecord[3];
@@ -16,22 +17,51 @@ uint8_t Dcm_Service_WriteMemoryByAddress(uint8_t *requestMessageData, uint8_t re
 				return 0x00;
 			}
 
+	//NRC 0x13
+    //length of the message is wrong
+	if((requestMessageLength < 6)||(requestMessageLength > 8))
+	            {
+	                responseData[0] = NEGATIVE_RESPONSE_SID;
+	    	        responseData[1] = 0x13;
+
+	    	            return 0x00;
+	            }
+
 	uint8_t responseDataIndex = 0;
+
+	//create positive response
 	responseData[responseDataIndex++] = DCM_SERVICE_ID_WRITE_MEMORY_BY_ADDRESS_RESPONSE_SID;
 
-	uint8_t requestDataIndex = 2;
+	uint8_t requestDataIndex = 1;
+
+	uint8_t addressAndLengthFormatIdentifier = requestMessageData[requestDataIndex++];
+
 	uint8_t addressHighByte = requestMessageData[requestDataIndex++];
 	uint8_t addressLowByte = requestMessageData[requestDataIndex++];
 	uint16_t memoryAddress = ((uint16_t)addressHighByte << 8) + addressLowByte;
 
 	uint8_t memorySize = requestMessageData[requestDataIndex++];
 
+	//NRC 0x31
+	//the memorySize parameter value in the request message is not supported by the server
+	//the specified addressAndLengthFormatIdentifier is not valid
+	//the memorySize parameter value in the request message is zero
+	if((memorySize > 2) || (memorySize == 0) || (addressAndLengthFormatIdentifier != 0x12))
+		{
+		    responseData[0] = NEGATIVE_RESPONSE_SID;
+	    	responseData[1] = 0x31;
+
+	    	    return 0x00;
+		}
+
+	//write data in a vector
     for(uint8_t DataIndex = 0; DataIndex < memorySize; DataIndex++){
 
 		dataRecord[DataIndex] = requestMessageData[requestDataIndex+DataIndex];
 
 	}
 
+    //complete the positive response
 	responseData[responseDataIndex++] = requestMessageData[1];
 	responseData[responseDataIndex++] = requestMessageData[2];
     responseData[responseDataIndex++] = requestMessageData[3];
